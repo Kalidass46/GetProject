b962d5a8a8620bd56784c5a4300b31823c0f0a5e
*Kalidass46* <*kalidass706010@gmail.com*>
2024-10-09 16:58:54 +0530

    cucumber project

--- src/test/java/org/jvm/JvmReport.java
+++ src/test/java/org/jvm/JvmReport.java
@@ -1,7 +1,11 @@
 package org.jvm;
 
 import java.io.File;
-import java.lang.module.Configuration;
+import java.util.LinkedList;
+import java.util.List;
+
+import net.masterthought.cucumber.Configuration;
+import net.masterthought.cucumber.ReportBuilder;
 
 public class JvmReport {
 
@@ -9,10 +13,21 @@ public class JvmReport {
 		
 		//	1.Mention target folder location-File
 		
-		File f=new File("C:\\Users\\Admin\\eclipse-workspace\\Cucumber\\org.jvm\\JvmReport.java");
+		File f=new File("C:\\Users\\Admin\\eclipse-workspace\\Cucumber\\Jvm");
 
 		//	2.Add details to the report using configuration class
-		//	addClassifications("point1","point2")
+		
+		Configuration c = new Configuration(f, "Adactin Hotel");
+		c.addClassifications("platform name","android");
+		c.addClassifications("platform version","windows 10");
+		c.addClassifications("browser name","chrome");
+		c.addClassifications("browser version","129");
+		
+		List<String> li = new LinkedList<String>();
+		li.add(jsonpath);
+		
+		ReportBuilder r = new ReportBuilder(li, c);
+		r.generateReports();
 		
 		
 		
